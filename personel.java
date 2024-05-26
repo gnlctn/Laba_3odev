3d834ea72cd077a2d1b156ace479ff11665c9f51
gnlctn <gnl.ctn07@gmail.com>
2024-05-26 15:22:14 +0300

    xml json yml eklendi

---
+++ personel.java
@@ -0,0 +1,11 @@
+public class personel {
+    String name;
+    String surname;
+    int age;
+    String city;
+    int street;
+    int no;
+    String mail;
+    String telefon;
+    String department;
+}
